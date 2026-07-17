0b9c609ad34ad3fc51e1b5eacd92c8fc2492df0a
Ansh Gupta <anshguptaiq98@gmail.com>
2026-07-18 04:41:51 +0530

    Day 7: Completed nested loops and pattern printing

--- Week1/Day6/08_continue_program.py
+++ Week1/Day6/08_continue_program.py
@@ -1,9 +1,7 @@
-# for number in range(1,11):
+for number in range(1,11):
 
-#     if number == 6:
-#         continue
+    if number == 6:
+        continue
 
-#     print(number)
-
-for number in range(3,3):
     print(number)
+
---
+++ Week1/Day7/14_practice_pattern.py
@@ -0,0 +1,27 @@
+# Continuous number pattern
+# ch = 1
+# for i in range(1,5):
+#     for j in range(i):
+#         print(ch , end ="")
+#         ch += 1
+#     print()    
+
+# Continuous alphabet pattern
+# ch = 65
+# for i in range(1,5):
+#     for j in range(i):
+#         print(chr(ch) , end ="")
+#         ch += 1
+#     print()    
+
+# Reverse star pattern
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*" , end = "")
+#     print()    
+
+# Reverse number pattern
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print(5-j , end = "")
+#     print()
