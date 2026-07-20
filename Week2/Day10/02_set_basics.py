f739f1d390b845a47e47adc95139beee93c0b960
Ansh Gupta <anshguptaiq98@gmail.com>
2026-07-21 03:35:39 +0530

    Day 10: Completed tuples and sets practice

---
+++ Week2/Day10/02_set_basics.py
@@ -0,0 +1,7 @@
+abc = {10,20,30}
+abc.add(40)
+print(abc)
+abc.remove(20)
+print(abc)
+abc.discard(500)
+print(abc)
---
+++ Week2/Day10/04_set_operations.py
@@ -0,0 +1,7 @@
+a = {10,20,30}
+b = {30,40,50}
+
+print(a|b)
+print(a&b)
+print(a-b)
+print(b-a)
